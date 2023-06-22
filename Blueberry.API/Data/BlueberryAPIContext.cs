eb627ba8ef8b2b1aca42fb029cc48146b1d763fe
DeenoRepository <deenovi.misc@gmail.com>
2023-06-22 13:03:17 +0700

    Add and initialize new project Blueberry.API

---
+++ Blueberry.API/Data/BlueberryAPIContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blueberry.API.Data
+{
+    public class BlueberryAPIContext : DbContext
+    {
+        public BlueberryAPIContext(DbContextOptions<BlueberryAPIContext> options)
+            : base(options) { }
+    }
+}
